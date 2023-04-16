d9e60892c15452a99d126026900d1a959104d876
Giovanni Criscione <giovanni2001rg@gmail.com>
2023-04-16 13:38:28 +0200

    Installato:  - firebase  - vue-typescript-import-dts funzionalità:  - middleware di navigazione  - configurazione db firebase

---
+++ src/firebase/index.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+//configurazione del database di firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBvr7GrFrPJ2ieCp8Vg6Tr8mQcg3qRy4MQ",
+  authDomain: "cookshare-a9ce4.firebaseapp.com",
+  projectId: "cookshare-a9ce4",
+  storageBucket: "cookshare-a9ce4.appspot.com",
+  messagingSenderId: "322994241297",
+  appId: "1:322994241297:web:685c2d3e895c23917a56b0",
+  measurementId: "G-B27EMYN1R3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export{ auth }
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,28 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import { auth } from '@/firebase';
 
 const routes: Array<RouteRecordRaw> = [
-  {
+  { //path home
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: {
+      requiresAuth: true
+    }
   },
-  {
+  { //path about
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import('../views/AboutView.vue'),
+    meta: {
+      requiresAuth: true
+    }
+  },
+  { //path login
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/LoginView.vue')
   }
 ]
 
@@ -21,5 +30,28 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+
+// middleware di navigazione per controllore che l'utente che naviga abbia i permessi
+// per accedere a determinate pagine 
+router.beforeEach((to, from, next)=>{
+  
+  // se un'utente loggato cerca di andare alla pagina di login, viene mandato alla pagine home
+  if(to.path === '/login' && auth.currentUser){
+    alert("sei già loggato!");
+    next('/');
+    return;
+  }
+
+  // se un utente non loggato cerca di andare in una pagina che richiede il login, viene mandato al login
+  if(to.matched.some(record => record.meta.requiresAuth) && !auth.currentUser){
+    alert("devi loggarti!");
+    next('/login');
+    return;
+  }
+
+  // altrimenti la navigazione prosegue normalmente
+  next();
+});
 
 export default router
