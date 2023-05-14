9ee65cc246ef83cc1b4fd679627adda294dca750
Giovanni Criscione <giovanni2001rg@gmail.com>
2023-05-14 12:05:16 +0200

    Configurazione PWA

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -2,31 +2,45 @@
 
 import { register } from 'register-service-worker'
 
+const CACHE_NAME = "CACHE_COOKSHARE";
+const STATIC_CACHE_URLS = ["/", ];
+
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
-    ready () {
+    ready() {
       console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB'
+        'CookShare: L\'app viene servita dalla cache tramite un service worker.\n' +
+        'Per maggiori dettagli, visita https://goo.gl/AFskqB'
       )
     },
-    registered () {
-      console.log('Service worker has been registered.')
+    registered() {
+      console.log('CookShare: Il service worker è stato registrato.')
     },
-    cached () {
-      console.log('Content has been cached for offline use.')
+    cached() {
+      console.log('CookShare: I contenuti sono stati messi in cache per l\'uso offline.')
+      return caches.open('app-cache').then(cache => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/manifest.json',
+          '/service-worker.js',
+          '/robots.txt',
+        ]);
+      });
     },
-    updatefound () {
-      console.log('New content is downloading.')
+    updatefound() {
+      console.log('CookShare: Si stanno scaricando nuovi contenuti.')
     },
-    updated () {
-      console.log('New content is available; please refresh.')
+    updated() {
+      console.log('CookShare: Sono disponibili nuovi contenuti, si prega di aggiornare la pagina.')
     },
-    offline () {
-      console.log('No internet connection found. App is running in offline mode.')
+    offline() {
+      console.log(
+        'CookShare: Nessuna connessione Internet trovata. L\'app sta funzionando in modalità offline.'
+      )
     },
-    error (error) {
-      console.error('Error during service worker registration:', error)
+    error(error) {
+      console.error('CookShare: Errore durante la registrazione del service worker:', error)
     }
   })
 }
